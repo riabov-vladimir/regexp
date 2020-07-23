e5ddd827589d55f01a7b13571a824c222bd83ca3
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-07-23 15:39:09 +0300

    for loop

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from pprint import pprint
-# читаем адресную книгу в формате CSV в список contacts_list
+from phone_number_formatter.phone_number_replacer import phone_number_replacer
 import csv
+from pprint import pprint
 
-
-with open("C:/Users/79055/PycharmProjects/regexp/data/phonebook_raw.csv") as f:
+with open("C:/Users/79055/PycharmProjects/regexp/data/phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-pprint(contacts_list)
 
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
+phone_number_replacer(contacts_list)
+
+new_contacts_list = []
 
 
-# TODO 2: сохраните получившиеся данные в другой файл
+for contact in contacts_list:
+    if len(contact[0].split()) == 3:
+        temp = contact[0].split()
+        contact[0] = temp[0]
+        contact[1] = temp[1]
+        contact[2] = temp[2]
+    elif len(contact[1].split()) == 2:
+        temp = contact[1].split()
+        contact[1] = temp[0]
+        contact[2] = temp[1]
+    elif len(contact[0].split()) == 2:
+        temp = contact[0].split()
+        contact[0] = temp[0]
+        contact[1] = temp[1]
 
-# код для записи файла в формате CSV
+contacts_list1 = contacts_list.copy()
 
-with open("phonebook.csv", "w") as f:
-    datawriter = csv.writer(f, delimiter=',')
-    # Вместо contacts_list подставьте свой список
-    datawriter.writerows(contacts_list)
+for contact in contacts_list1:
+    contacts_list1.remove(contact)
+    for y in contacts_list1:
+        if y[0] == contact[0]:
+            print(y[0], contact[0])
+            print('---------------------------------------------------')

e05edc704d595aff280f4a700a4c85953cd0859f
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-07-23 11:25:35 +0300

    full name arrangment - done regexp - corrected

--- phone_number_formatter/phone_number_regexp.py
+++ phone_number_formatter/phone_number_regexp.py
@@ -5,7 +5,7 @@ def format_phone_number(phone_number: str) -> str:
 	"""
 	Форматирование телефонных номеров при помощи регулярного выражения
 	:param phone_number:
-	:return: phone_number в формате +7(999)999-99-99
+	:return: phone_number в формате +7(999)999-99-99 доб.9999
 	"""
 
 	phone_regex = r"(\+?[+78])\s?\(?(\d{3})\)?[- ]?(\d{3})\-?(\d{2})\-?(\d{2})\s?((\(?(доб.)\s(\d+)\)?)?)"
@@ -13,13 +13,8 @@ def format_phone_number(phone_number: str) -> str:
 	pattern = re.compile(phone_regex)
 
 	if 'доб' in phone_number:
-		phone_number_formatted = pattern.sub(r'+7(\2)\3-\4-\5 (доб. \9)', phone_number)
+		phone_number_formatted = pattern.sub(r'+7(\2)\3-\4-\5 доб.\9', phone_number)
 	else:
 		phone_number_formatted = pattern.sub(r'+7(\2)\3-\4-\5', phone_number)
 
 	return phone_number_formatted
-
-
-if __name__ == '__main__':
-	phone_number = '+7 (495) 913-1111'  # (доб. 0792)'
-	print(format_phone_number(phone_number))
